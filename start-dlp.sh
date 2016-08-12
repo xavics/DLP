575367e8da87cc6ce78f154aea919b168dc1d453
xavics <xacosa@gmail.com>
2016-08-12 10:33:58 +0200

    Added Script to start dlp with the apps server of Liquid Galaxy Lab

---
+++ start-dlp.sh
@@ -0,0 +1,8 @@
+# This script is not needed for the project.
+#
+# Is it needed for the correct integration with other projects in our Liquid Galaxy Server
+#!/bin/bash
+
+cd /home/lg/Desktop/lglab/gsoc16/DLP
+source ~/Envs/pilt/bin/activate
+python manage.py rundlp $1
